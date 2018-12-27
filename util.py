898c47ebf2ce5d066610d2b191a2f980a1e87795
fangpindar <32450789+fangpindar@users.noreply.github.com>
2018-12-27 11:48:53 +0800

    util change method 抵免

--- util.py
+++ util.py
@@ -31,7 +31,7 @@ def worm(acc, pas):
         if (i == 0 or i == 1):
             continue
         GenMustListtmep = GenMustList[i].find_elements_by_tag_name('td')
-        if (GenMustListtmep[4].text == "通過" or int(GenMustListtmep[4].text) >= 60 or GenMustListtmep[4].text == "抵免"):
+        if (GenMustListtmep[4].text == "通過" or GenMustListtmep[4].text == "抵免" or int(GenMustListtmep[4].text) >= 60):
             answer["通識基礎"].append(GenMustListtmep[1].text)
 
     CollegeMustListView = browser.find_element_by_id(
@@ -41,7 +41,7 @@ def worm(acc, pas):
         if (i == 0 or i == 1):
             continue
         CollegeMustListViewtmep = CollegeMustListView[i].find_elements_by_tag_name('td')
-        if (CollegeMustListViewtmep[4].text == "通過" or int(CollegeMustListViewtmep[4].text) >= 60 or CollegeMustListViewtmep[4].text == "抵免"):
+        if (CollegeMustListViewtmep[4].text == "通過" or CollegeMustListViewtmep[4].text == "抵免" or int(CollegeMustListViewtmep[4].text) >= 60):
             answer["院系必修"].append(CollegeMustListViewtmep[1].text)
 
     CollegeProListView = browser.find_element_by_id(
@@ -51,7 +51,7 @@ def worm(acc, pas):
         if (i == 0 or i == 1):
             continue
         CollegeProListViewtmep = CollegeProListView[i].find_elements_by_tag_name('td')
-        if (CollegeProListViewtmep[4].text == "通過" or int(CollegeProListViewtmep[4].text) >= 60 or CollegeProListViewtmep[4].text == "抵免"):
+        if (CollegeProListViewtmep[4].text == "通過" or CollegeProListViewtmep[4].text == "抵免" or int(CollegeProListViewtmep[4].text) >= 60):
             answer["本系專業選修"].append(CollegeProListViewtmep[1].text)
 
     NoCollegeProListView = browser.find_element_by_id(
@@ -61,7 +61,7 @@ def worm(acc, pas):
         if (i == 0 or i == 1):
             continue
         NoCollegeProListViewtmep = NoCollegeProListView[i].find_elements_by_tag_name('td')
-        if (NoCollegeProListViewtmep[4].text == "通過" or int(NoCollegeProListViewtmep[4].text) >= 60 or NoCollegeProListViewtmep[4].text == "抵免"):
+        if (NoCollegeProListViewtmep[4].text == "通過" or NoCollegeProListViewtmep[4].text == "抵免" or int(NoCollegeProListViewtmep[4].text) >= 60):
             answer["非本系專業選修"].append(NoCollegeProListViewtmep[1].text)
 
     GenSelectListView = browser.find_element_by_id(
@@ -71,7 +71,7 @@ def worm(acc, pas):
         if (i == 0 or i == 1):
             continue
         GenSelectListViewtmep = GenSelectListView[i].find_elements_by_tag_name('td')
-        if (GenSelectListViewtmep[4].text == "通過" or int(GenSelectListViewtmep[4].text) >= 60 or GenSelectListViewtmep[4].text == "抵免"):
+        if (GenSelectListViewtmep[4].text == "通過" or GenSelectListViewtmep[4].text == "抵免" or int(GenSelectListViewtmep[4].text) >= 60):
             answer["通識選修"].append(GenSelectListViewtmep[1].text)
 
     return answer
