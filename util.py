f46d29750f45f4d04e237d73ebe3bd788dbd81a3
fangpindar <32450789+fangpindar@users.noreply.github.com>
2019-01-03 11:50:49 +0800

    add try
    87鄧宇淇沒有修過選修阿

--- util.py
+++ util.py
@@ -23,55 +23,68 @@ def worm(acc, pas):
               "非本系專業選修": [],
               "通識選修": []
               }
+    try:
+        GenMustList = browser.find_element_by_id(
+            "ctl00_MainContent_GenMustListView_tableHeader").find_elements_by_tag_name('tr')
 
-    GenMustList = browser.find_element_by_id(
-        "ctl00_MainContent_GenMustListView_tableHeader").find_elements_by_tag_name('tr')
+        for i in range(len(GenMustList)):
+            if (i == 0 or i == 1):
+                continue
+            GenMustListtmep = GenMustList[i].find_elements_by_tag_name('td')
+            if (GenMustListtmep[4].text == "通過" or GenMustListtmep[4].text == "抵免" or int(GenMustListtmep[4].text) >= 60):
+                answer["通識基礎"].append(GenMustListtmep[1].text)
+    except :
+        print('無通識基礎修課紀錄!')
+        
+    try:
+        CollegeMustListView = browser.find_element_by_id(
+            "ctl00_MainContent_CollegeMustListView_tableHeader").find_elements_by_tag_name('tr')
 
-    for i in range(len(GenMustList)):
-        if (i == 0 or i == 1):
-            continue
-        GenMustListtmep = GenMustList[i].find_elements_by_tag_name('td')
-        if (GenMustListtmep[4].text == "通過" or GenMustListtmep[4].text == "抵免" or int(GenMustListtmep[4].text) >= 60):
-            answer["通識基礎"].append(GenMustListtmep[1].text)
+        for i in range(len(CollegeMustListView)):
+            if (i == 0 or i == 1):
+                continue
+            CollegeMustListViewtmep = CollegeMustListView[i].find_elements_by_tag_name('td')
+            if (CollegeMustListViewtmep[4].text == "通過" or CollegeMustListViewtmep[4].text == "抵免" or int(CollegeMustListViewtmep[4].text) >= 60):
+                answer["院系必修"].append(CollegeMustListViewtmep[1].text)
+    except :
+        print('無院系必修修課紀錄!')
+        
+    try:
+        CollegeProListView = browser.find_element_by_id(
+            "ctl00_MainContent_CollegeProListView_tableHeader").find_elements_by_tag_name('tr')
 
-    CollegeMustListView = browser.find_element_by_id(
-        "ctl00_MainContent_CollegeMustListView_tableHeader").find_elements_by_tag_name('tr')
+        for i in range(len(CollegeProListView)):
+            if (i == 0 or i == 1):
+                continue
+            CollegeProListViewtmep = CollegeProListView[i].find_elements_by_tag_name('td')
+            if (CollegeProListViewtmep[4].text == "通過" or CollegeProListViewtmep[4].text == "抵免" or int(CollegeProListViewtmep[4].text) >= 60):
+                answer["本系專業選修"].append(CollegeProListViewtmep[1].text)
+    except :
+        print('無本系專業選修修課紀錄!')
 
-    for i in range(len(CollegeMustListView)):
-        if (i == 0 or i == 1):
-            continue
-        CollegeMustListViewtmep = CollegeMustListView[i].find_elements_by_tag_name('td')
-        if (CollegeMustListViewtmep[4].text == "通過" or CollegeMustListViewtmep[4].text == "抵免" or int(CollegeMustListViewtmep[4].text) >= 60):
-            answer["院系必修"].append(CollegeMustListViewtmep[1].text)
+    try:
+        NoCollegeProListView = browser.find_element_by_id(
+            "ctl00_MainContent_NoCollegeProListView_tableHeader").find_elements_by_tag_name('tr')
 
-    CollegeProListView = browser.find_element_by_id(
-        "ctl00_MainContent_CollegeProListView_tableHeader").find_elements_by_tag_name('tr')
+        for i in range(len(NoCollegeProListView)):
+            if (i == 0 or i == 1):
+                continue
+            NoCollegeProListViewtmep = NoCollegeProListView[i].find_elements_by_tag_name('td')
+            if (NoCollegeProListViewtmep[4].text == "通過" or NoCollegeProListViewtmep[4].text == "抵免" or int(NoCollegeProListViewtmep[4].text) >= 60):
+                answer["非本系專業選修"].append(NoCollegeProListViewtmep[1].text)
+    except :
+        print('無本系專業選修修課紀錄!')
 
-    for i in range(len(CollegeProListView)):
-        if (i == 0 or i == 1):
-            continue
-        CollegeProListViewtmep = CollegeProListView[i].find_elements_by_tag_name('td')
-        if (CollegeProListViewtmep[4].text == "通過" or CollegeProListViewtmep[4].text == "抵免" or int(CollegeProListViewtmep[4].text) >= 60):
-            answer["本系專業選修"].append(CollegeProListViewtmep[1].text)
+    try:
+        GenSelectListView = browser.find_element_by_id(
+            "ctl00_MainContent_GenSelectListView_tableHeader").find_elements_by_tag_name('tr')
 
-    NoCollegeProListView = browser.find_element_by_id(
-        "ctl00_MainContent_NoCollegeProListView_tableHeader").find_elements_by_tag_name('tr')
-
-    for i in range(len(NoCollegeProListView)):
-        if (i == 0 or i == 1):
-            continue
-        NoCollegeProListViewtmep = NoCollegeProListView[i].find_elements_by_tag_name('td')
-        if (NoCollegeProListViewtmep[4].text == "通過" or NoCollegeProListViewtmep[4].text == "抵免" or int(NoCollegeProListViewtmep[4].text) >= 60):
-            answer["非本系專業選修"].append(NoCollegeProListViewtmep[1].text)
-
-    GenSelectListView = browser.find_element_by_id(
-        "ctl00_MainContent_GenSelectListView_tableHeader").find_elements_by_tag_name('tr')
-
-    for i in range(len(GenSelectListView)):
-        if (i == 0 or i == 1):
-            continue
-        GenSelectListViewtmep = GenSelectListView[i].find_elements_by_tag_name('td')
-        if (GenSelectListViewtmep[4].text == "通過" or GenSelectListViewtmep[4].text == "抵免" or int(GenSelectListViewtmep[4].text) >= 60):
-            answer["通識選修"].append(GenSelectListViewtmep[1].text)
-
+        for i in range(len(GenSelectListView)):
+            if (i == 0 or i == 1):
+                continue
+            GenSelectListViewtmep = GenSelectListView[i].find_elements_by_tag_name('td')
+            if (GenSelectListViewtmep[4].text == "通過" or GenSelectListViewtmep[4].text == "抵免" or int(GenSelectListViewtmep[4].text) >= 60):
+                answer["通識選修"].append(GenSelectListViewtmep[1].text)
+    except :
+        print('無通識選修修課紀錄!')
     return answer
